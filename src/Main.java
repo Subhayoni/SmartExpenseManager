cebc6f05b248b5e58785636c17a224aedd3e0c4a
Subhayoni <subhayoni@gmail.com>
2026-03-22 17:06:00 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// entry point, will build this out later
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting Expense Manager...");
+        // TODO: wire up the menu here
+    }
+}
